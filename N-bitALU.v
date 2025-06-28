a86baa6df6cc6d7304d492170157f83ec2658093
youssefsameh99 <ysameh487@gmail.com>
2025-06-28 18:10:20 +0300

    Update N-bitALU.v

--- N-bitALU.v
+++ N-bitALU.v
@@ -7,7 +7,7 @@ module NbitALU #(parameter N=3)(
         case (opcode)
         2'b00 : out = in1 + in0;
         2'b01 : out = in1 | in0;
-        2'b10 : out = in0 - in1;
+        2'b10 : out = in1 - in0;
         2'b11 : out = in1 ^ in0;         
         endcase
     end
