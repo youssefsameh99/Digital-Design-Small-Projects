ea08af36320ce05d3b7d2caec8dd35b52c2c3d90
youssefsameh99 <ysameh487@gmail.com>
2025-06-28 18:12:41 +0300

    Update 2x1Mux.v

--- 2x1Mux.v
+++ 2x1Mux.v
@@ -1,3 +1,19 @@
+//------------------------------------------------------------------------------
+// Module Name: mux2to1
+// Description: This module implements a 2-to-1 multiplexer. It selects one of
+//              the two input signals (in0 or in1) based on the value of the
+//              selection line 'sel'.
+//
+// Inputs:
+//   in0 - First input
+//   in1 - Second input
+//   sel - Select line (0 selects in0, 1 selects in1)
+//
+// Output:
+//   out - Output that reflects the selected input
+//------------------------------------------------------------------------------
+
+
 module mux2to1(
     input in0,in1,sel,
     output out
