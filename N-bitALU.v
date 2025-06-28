93198e808fe488b2053bdf2db7de06e07e816571
youssefsameh99 <ysameh487@gmail.com>
2025-06-28 18:13:19 +0300

    Update N-bitALU.v

--- N-bitALU.v
+++ N-bitALU.v
@@ -1,3 +1,26 @@
+//------------------------------------------------------------------------------
+// Module Name: NbitALU
+// Description: This module implements a simple N-bit Arithmetic Logic Unit (ALU)
+//              that performs operations on two N-bit input values based on a 
+//              2-bit opcode.
+//
+// Parameters:
+//   N - Bit-width of the input and output buses (default is 3)
+//
+// Inputs:
+//   in1    - First N-bit operand
+//   in0    - Second N-bit operand
+//   opcode - 2-bit control signal to select the operation:
+//              00 -> Addition (in1 + in0)
+//              01 -> Bitwise OR (in1 | in0)
+//              10 -> Subtraction (in1 - in0)
+//              11 -> Bitwise XOR (in1 ^ in0)
+//
+// Output:
+//   out - N-bit result of the selected operation
+//------------------------------------------------------------------------------
+
+
 module NbitALU #(parameter N=3)(
     input [N-1:0] in1,in0,
     input [1:0] opcode,
